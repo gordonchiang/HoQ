1f4a0fb6652693bc0ecdf25e990a1e79f2fab719
Gordon Chiang <gchiang@uvic.ca>
2022-03-06 23:52:07 -0800

    Send sample HL7 message via HTTP using HAPI example

--- src/main/java/ca/uvic/hoq/App.java
+++ src/main/java/ca/uvic/hoq/App.java
@@ -1,14 +1,26 @@
 package ca.uvic.hoq;
 
 import ca.uhn.hl7v2.DefaultHapiContext;
-import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.app.Connection;
+import ca.uhn.hl7v2.app.Initiator;
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.model.v24.message.ADT_A01;
 import ca.uhn.hl7v2.model.v24.segment.MSH;
 import ca.uhn.hl7v2.model.v24.segment.PID;
 import ca.uhn.hl7v2.parser.Parser;
+import ca.uhn.hl7v2.hoh.hapi.client.HohClientSimple;
+import ca.uhn.hl7v2.hoh.api.ISendable;
+import ca.uhn.hl7v2.hoh.api.IReceivable;
+import ca.uhn.hl7v2.hoh.hapi.api.MessageSendable;
+import ca.uhn.hl7v2.hoh.api.MessageMetadataKeys;
 
 public class App {
+
+	private static final String HOST = "localhost";
+	private static final String URI = "/";
+	private static final int PORT_NUMBER = 8888;
 
 	private static HapiContext context = new DefaultHapiContext();
 
@@ -48,6 +60,22 @@ public class App {
 		 * MSH|^~\&|TestSendingSystem||||200701011539||ADT^A01^ADT A01||||123
 		 * PID|||123456||Doe^John
 		 */
+		
+		HohClientSimple client = new HohClientSimple(HOST, PORT_NUMBER, URI, parser);
+		
+		// The MessageSendable provides the message to send 
+		ISendable sendable = new MessageSendable(adt);
+		
+		// sendAndReceive actually sends the message
+		IReceivable<Message> receivable = client.sendAndReceiveMessage(sendable);
+
+		// receivavle.getRawMessage() provides the response
+		Message message = receivable.getMessage();
+		System.out.println("Response was:\n" + message.encode());
+
+		// IReceivable also stores metadata about the message
+		String remoteHostIp = (String) receivable.getMetadata().get(MessageMetadataKeys.REMOTE_HOST_ADDRESS);
+		System.out.println("From:\n" + remoteHostIp);
 	}
 
 }
