41f020c7e6c796b40cc561311e43b4d976fdcae8
Gordon Chiang <gchiang@uvic.ca>
2022-03-07 22:00:32 -0800

    Send sample HL7 message using client

--- src/main/java/ca/uvic/hoq/Http3Client.java
+++ src/main/java/ca/uvic/hoq/Http3Client.java
@@ -14,6 +14,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.parser.Parser;
 import ca.uvic.hoq.Http3Server.PartialResponse;
 import io.quiche4j.Config;
 import io.quiche4j.ConfigBuilder;
@@ -26,6 +29,15 @@ import io.quiche4j.http3.Http3EventListener;
 import io.quiche4j.http3.Http3Header;
 import io.quiche4j.Quiche;
 import io.quiche4j.Utils;
+
+import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.model.v24.message.ADT_A01;
+import ca.uhn.hl7v2.model.v24.segment.MSH;
+import ca.uhn.hl7v2.model.v24.segment.PID;
+import ca.uhn.hl7v2.parser.Parser;
 
 public class Http3Client {
 
@@ -171,7 +183,7 @@ public class Http3Client {
 
                 System.out.println("! h3 conn is established");
 
-                final byte[] body = "Hello server, I'm client!".getBytes();
+                final byte[] body = HL7MessageGenerator().getBytes();
 
                 List<Http3Header> req = new ArrayList<>();
                 req.add(new Http3Header(":method", "GET"));
@@ -217,5 +229,49 @@ public class Http3Client {
         System.out.println("> conn is closed");
         System.out.println(conn.stats());
         socket.close();
+    }
+    
+    public final static String HL7MessageGenerator() {
+    	String encodedMessage = null;
+
+    	try {
+            ADT_A01 adt = new ADT_A01();
+    		adt.initQuickstart("ADT", "A01", "P");
+
+    		// Populate the MSH Segment
+    		MSH mshSegment = adt.getMSH();
+    		mshSegment.getSendingApplication().getNamespaceID().setValue("TestSendingSystem");
+    		mshSegment.getSequenceNumber().setValue("123");
+
+    		// Populate the PID Segment
+    		PID pid = adt.getPID();
+    		pid.getPatientName(0).getFamilyName().getSurname().setValue("Doe");
+    		pid.getPatientName(0).getGivenName().setValue("John");
+    		pid.getPatientIdentifierList(0).getID().setValue("123456");
+
+    		/*
+    		 * In a real situation, of course, many more segments and fields would be
+    		 * populated
+    		 */
+
+    		// Now, let's encode the message and look at the output
+    		HapiContext context = new DefaultHapiContext();
+    		Parser parser = context.getPipeParser();
+    		encodedMessage = parser.encode(adt);
+    		System.out.println("Printing ER7 Encoded Message:");
+    		System.out.println(encodedMessage);
+
+    		/*
+    		 * Prints:
+    		 * 
+    		 * MSH|^~\&|TestSendingSystem||||200701011539||ADT^A01^ADT A01||||123
+    		 * PID|||123456||Doe^John
+    		 */
+    	} catch (Exception e) {
+    		System.out.println("Error generating HL7 message: " + e);
+    		System.exit(1);
+    	}
+    	
+    	return encodedMessage;
     }
 }
