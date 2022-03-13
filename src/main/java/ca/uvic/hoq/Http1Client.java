746d9c443a1335ed30bdcc350b362e8a5b8c031e
Gordon Chiang <gchiang@uvic.ca>
2022-03-13 01:43:51 -0800

    Formatting

--- src/main/java/ca/uvic/hoq/Http1Client.java
+++ src/main/java/ca/uvic/hoq/Http1Client.java
@@ -23,76 +23,76 @@ import io.quiche4j.Utils;
 
 public class Http1Client {
 
-	private static HapiContext context = new DefaultHapiContext();
+  private static HapiContext context = new DefaultHapiContext();
 
-	public static void main(String[] args) throws Exception {
-		// Parse arguments
-		if (2 != args.length) {
-			System.out.println("Usage: ./http1.sh -c -t -u https://localhost:8888");
-			System.exit(1);
-		}
+  public static void main(String[] args) throws Exception {
+    // Parse arguments
+    if (2 != args.length) {
+      System.out.println("Usage: ./http1.sh -c -t -u https://localhost:8888");
+      System.exit(1);
+    }
 
-		final String url = args[0];
-		final URI uri;
-		final int port;
-		final String host;
-		try {
-			uri = new URI(url);
-			port = uri.getPort();
-			host = uri.getHost();
-		} catch (URISyntaxException e) {
-			System.out.println("Failed to parse URL " + url);
-			System.exit(1);
-			return;
-		}
+    final String url = args[0];
+    final URI uri;
+    final int port;
+    final String host;
+    try {
+      uri = new URI(url);
+      port = uri.getPort();
+      host = uri.getHost();
+    } catch (URISyntaxException e) {
+      System.out.println("Failed to parse URL " + url);
+      System.exit(1);
+      return;
+    }
 
-		final boolean enableTLS = args[1].equals("true") ? true : false;
+    final boolean enableTLS = args[1].equals("true") ? true : false;
 
-		// Create HL7 message
-		ADT_A01 adt = new ADT_A01();
-		adt.initQuickstart("ADT", "A01", "P");
+    // Create HL7 message
+    ADT_A01 adt = new ADT_A01();
+    adt.initQuickstart("ADT", "A01", "P");
 
-		// Populate the MSH Segment
-		MSH mshSegment = adt.getMSH();
-		mshSegment.getSendingApplication().getNamespaceID().setValue("TestSendingSystem");
-		mshSegment.getSequenceNumber().setValue("123");
+    // Populate the MSH Segment
+    MSH mshSegment = adt.getMSH();
+    mshSegment.getSendingApplication().getNamespaceID().setValue("TestSendingSystem");
+    mshSegment.getSequenceNumber().setValue("123");
 
-		// Populate the PID Segment
-		PID pid = adt.getPID();
-		pid.getPatientName(0).getFamilyName().getSurname().setValue("Doe");
-		pid.getPatientName(0).getGivenName().setValue("John");
-		pid.getPatientIdentifierList(0).getID().setValue("123456");
+    // Populate the PID Segment
+    PID pid = adt.getPID();
+    pid.getPatientName(0).getFamilyName().getSurname().setValue("Doe");
+    pid.getPatientName(0).getGivenName().setValue("John");
+    pid.getPatientIdentifierList(0).getID().setValue("123456");
 
-		/*
-		 * In a real situation, of course, many more segments and fields would be
-		 * populated
-		 */
+    /*
+     * In a real situation, of course, many more segments and fields would be
+     * populated
+     */
 
-		// Encode the message
-		HapiContext context = new DefaultHapiContext();
-		Parser parser = context.getPipeParser();
-		String encodedMessage = parser.encode(adt);
-		System.out.println("HL7 message: " + encodedMessage);
+    // Encode the message
+    HapiContext context = new DefaultHapiContext();
+    Parser parser = context.getPipeParser();
+    String encodedMessage = parser.encode(adt);
+    System.out.println("HL7 message: " + encodedMessage);
 
-		HohClientSimple client = new HohClientSimple(host, port, "/", parser);
+    HohClientSimple client = new HohClientSimple(host, port, "/", parser);
 
-		if (enableTLS) {
-			// Assign a socket factory which references the keystore
-			CustomCertificateTlsSocketFactory clientSocketFactory = new CustomCertificateTlsSocketFactory();
-			clientSocketFactory.setKeystoreFilename(Utils.copyFileFromJAR("keystore", "/keystore.pkcs12"));
-			clientSocketFactory.setKeystorePassphrase("hoqpassword");
-			client.setSocketFactory(clientSocketFactory);
-		}
+    if (enableTLS) {
+      // Assign a socket factory which references the keystore
+      CustomCertificateTlsSocketFactory clientSocketFactory = new CustomCertificateTlsSocketFactory();
+      clientSocketFactory.setKeystoreFilename(Utils.copyFileFromJAR("keystore", "/keystore.pkcs12"));
+      clientSocketFactory.setKeystorePassphrase("hoqpassword");
+      client.setSocketFactory(clientSocketFactory);
+    }
 
-		// The MessageSendable provides the message to send
-		ISendable sendable = new MessageSendable(adt);
+    // The MessageSendable provides the message to send
+    ISendable sendable = new MessageSendable(adt);
 
-		// sendAndReceive actually sends the message
-		IReceivable<Message> receivable = client.sendAndReceiveMessage(sendable);
+    // sendAndReceive actually sends the message
+    IReceivable<Message> receivable = client.sendAndReceiveMessage(sendable);
 
-		// receivable.getRawMessage() provides the response
-		Message message = receivable.getMessage();
-		System.out.println("Response: " + message.encode());
-	}
+    // receivable.getRawMessage() provides the response
+    Message message = receivable.getMessage();
+    System.out.println("Response: " + message.encode());
+  }
 
 }
--- src/main/java/ca/uvic/hoq/Http1Server.java
+++ src/main/java/ca/uvic/hoq/Http1Server.java
@@ -16,51 +16,49 @@ import io.quiche4j.Utils;
 
 public class Http1Server {
 
-	private static HapiContext context = new DefaultHapiContext();
+  private static HapiContext context = new DefaultHapiContext();
 
-	public static void main(String[] args) throws Exception {
-		// Parse arguments
-		if (2 != args.length) {
-			System.out.println("Usage: ./http1.sh -s -t -u 8888");
-			System.exit(1);
-		}
+  public static void main(String[] args) throws Exception {
+    // Parse arguments
+    if (2 != args.length) {
+      System.out.println("Usage: ./http1.sh -s -t -u 8888");
+      System.exit(1);
+    }
 
-		final String url = args[0];
-		final int port;
-		if (args[0].contains(":")) {
-			final String[] parts = args[0].split(":", 2);
-			port = Integer.parseInt(parts[1]);
-		} else {
-			port = Integer.parseInt(args[0]);
-		}
+    final String url = args[0];
+    final int port;
+    if (args[0].contains(":")) {
+      final String[] parts = args[0].split(":", 2);
+      port = Integer.parseInt(parts[1]);
+    } else {
+      port = Integer.parseInt(args[0]);
+    }
 
-		final boolean enableTLS = args[1].equals("true") ? true : false;
+    final boolean enableTLS = args[1].equals("true") ? true : false;
 
-		HapiSocketTlsFactoryWrapper hapiSocketFactory = null;
-		if (enableTLS) {
-			// Create a socket factory which references the keystore
-			CustomCertificateTlsSocketFactory serverSocketFactory = new CustomCertificateTlsSocketFactory();
-			serverSocketFactory.setKeystoreFilename(Utils.copyFileFromJAR("keystore", "/keystore.pkcs12"));
-			serverSocketFactory.setKeystorePassphrase("hoqpassword");
+    HapiSocketTlsFactoryWrapper hapiSocketFactory = null;
+    if (enableTLS) {
+      // Create a socket factory which references the keystore
+      CustomCertificateTlsSocketFactory serverSocketFactory = new CustomCertificateTlsSocketFactory();
+      serverSocketFactory.setKeystoreFilename(Utils.copyFileFromJAR("keystore", "/keystore.pkcs12"));
+      serverSocketFactory.setKeystorePassphrase("hoqpassword");
 
-			// The socket factory needs to be wrapped for use in HAPI
-			hapiSocketFactory = new HapiSocketTlsFactoryWrapper(serverSocketFactory);
-			context.setSocketFactory(hapiSocketFactory);
-		}
+      // The socket factory needs to be wrapped for use in HAPI
+      hapiSocketFactory = new HapiSocketTlsFactoryWrapper(serverSocketFactory);
+      context.setSocketFactory(hapiSocketFactory);
+    }
 
-		// Create an LLP instance
-		LowerLayerProtocol llp = new Hl7OverHttpLowerLayerProtocol(ServerRoleEnum.SERVER);
+    // Create an LLP instance
+    LowerLayerProtocol llp = new Hl7OverHttpLowerLayerProtocol(ServerRoleEnum.SERVER);
 
-		// Create the server
-		PipeParser parser = PipeParser.getInstanceWithNoValidation();
-		context.setLowerLayerProtocol(llp);
-		HL7Service server = enableTLS
-			? context.newServer(port, enableTLS) // Start a server listening at port with a pipe parser
-			: new SimpleServer(port, llp, parser); // Start a server with the llp and parser
+    // Create the server
+    PipeParser parser = PipeParser.getInstanceWithNoValidation();
+    context.setLowerLayerProtocol(llp);
+    HL7Service server = enableTLS ? context.newServer(port, enableTLS) : new SimpleServer(port, llp, parser);
 
-		// Register an application to the server and start it
-		server.registerApplication("*", "*", new MyApplication());
-		server.start();
-	}
+    // Register an application to the server and start it
+    server.registerApplication("*", "*", new MyApplication());
+    server.start();
+  }
 
 }
