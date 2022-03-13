6509578e39f2d3114cedb0cd6c72d1992fc4e2c6
Gordon Chiang <gchiang@uvic.ca>
2022-03-13 03:44:16 -0700

    Fix start script

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 print_usage() {
-  printf "Usage:\nHTTP/1.1 Server: ./run.sh -s -u https://localhost:8888 -v 1 (-t)\nHTTP/1.1 Client: ./run.sh -c -u 8888 -v 1 (-t)\nQUIC Server: ./run.sh -s -u 8888 -v 3\nQUIC Client: ./run.sh -c -u https://localhost:8888 -v 3\n"
+  printf "Usage:\nHTTP/1.1 Server: ./run.sh -s -u https://localhost:8888 -v 1 -t\nHTTP/1.1 Client: ./run.sh -c -u https://localhost:8888 -v 1 -t\nQUIC Server: ./run.sh -s -u https://localhost:8888 -v 3\nQUIC Client: ./run.sh -c -u https://localhost:8888 -v 3\n-t optional for HTTP/1.1+TLS\n"
 }
 
-if [ $OPTIND -eq 1 ]; then
-  print_usage;
-  exit 1;
+if [ $# -eq 0 ];
+  then
+    print_usage;
+    exit 1;
 fi
 
 ENABLE_TLS="false"
@@ -23,7 +24,16 @@ while getopts 'cstv:u:' flag; do
   esac
 done
 
-java \
-    -cp target/hoq-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-    ca.uvic.hoq.Http${VERSION}${MODE} $URL $ENABLE_TLS
+if [ $VERSION -eq "1" ];
+  then
+    java \
+      -cp target/hoq-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+      ca.uvic.hoq.Http${VERSION}${MODE} $URL $ENABLE_TLS;
+  else
+    java \
+      -cp target/hoq-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+      ca.uvic.hoq.Http${VERSION}${MODE} $URL;
+fi
+
+
 
