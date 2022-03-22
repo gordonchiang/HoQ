7bd9b00132d08742c403949048f360416edc0b39
Gordon Chiang <gchiang@uvic.ca>
2022-03-22 16:36:56 -0700

    Update MyApplication error response

--- src/main/java/ca/uvic/hoq/MyApplication.java
+++ src/main/java/ca/uvic/hoq/MyApplication.java
@@ -10,6 +10,7 @@ import ca.uhn.hl7v2.parser.PipeParser;
 import ca.uhn.hl7v2.validation.ValidationContext;
 import ca.uhn.hl7v2.validation.impl.ValidationContextFactory;
 import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.AcknowledgmentCode;
 import ca.uhn.hl7v2.protocol.ReceivingApplication;
 import ca.uhn.hl7v2.protocol.ReceivingApplicationException;
 
@@ -51,7 +52,7 @@ public class MyApplication implements ReceivingApplication {
     // Respond with AE response code and HTTP 500 status code
     if (somethingFailed) {
       try {
-        response = theMessage.generateACK("AE", new HL7Exception("There was a problem!"));
+        response = theMessage.generateACK(AcknowledgmentCode.AE, new HL7Exception("MyApplication experienced an error when processing the input HL7 message"));
       } catch (IOException e) {
         throw new ReceivingApplicationException(e);
       }
