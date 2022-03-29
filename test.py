214dbbf719189e5815f33b2a12b53054c7f81089
Gordon Chiang <gchiang@uvic.ca>
2022-03-28 20:31:17 -0700

    Update test script

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 from sys import argv
+from time import sleep, localtime
 
 from mininet.net import Mininet
 from mininet.node import Controller
@@ -11,13 +12,13 @@ from mininet.log import setLogLevel, info
 
 class TestTopo(Topo):
   def build(self, n=2): # Default: 2 hosts connected to 1 switch: h1--s1--h2
-      switch = self.addSwitch('s1')
-      for h in range(n):
-        # Add host
-        host = self.addHost('h%s' % (h + 1))
+    switch = self.addSwitch('s1')
+    for h in range(n):
+      # Add hosts
+      host = self.addHost('h%s' % (h + 1))
 
-        # 10 Mbps, 5ms delay, no packet loss
-        self.addLink(host, switch, bw=10, delay='5ms', loss=0)
+      # 10 Mbps, 5ms delay, no packet loss
+      self.addLink(host, switch, bw=10, delay='5ms', loss=0)
 
 def main():
   topo = TestTopo() 
@@ -25,14 +26,27 @@ def main():
 
   net.start()
 
-  info("Dumping host connections\n")
+  info('Dumping host connections\n')
   dumpNodeConnections(net.hosts)
 
-  h1, h2  = net.hosts[0], net.hosts[1]
+  h1, h2, s1  = net.hosts[0], net.hosts[1], net.switches[0]
 
-  print(h2.cmd('./run.sh -s -u https://{}:8888 -v 3 &'.format(h2.IP())))
+  now = localtime()
+  filename = '{}{}{}{}{}{}_s1_dump.pcap'.format(now[0], now[1], now[2], now[3], now[4], now[5])
+  info('Recording packets: {}\n'.format(filename))
+  s1_pcap = s1.popen(['wireshark', '-i', 's1-eth1', '-i', 's1-eth2', '-f', 'port 8888', '-k', '-w', './dumps/{}'.format(filename)]) # TODO: -f doesn't seem to work
 
+  sleep(10)
+
+  info('Starting server\n')
+  h2.cmd('./run.sh -s -u https://{}:8888 -v 3 &'.format(h2.IP()))
+
+  info('Starting client\n')
   print(h1.cmd('./run.sh -c -u https://{}:8888 -v 3'.format(h2.IP())))
+
+  sleep(10)
+
+  s1_pcap.terminate()
 
   net.stop()
 
