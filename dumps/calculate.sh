7be5884accb84ec08cabfd05e6713e3d781ae7ac
Gordon Chiang <gchiang@uvic.ca>
2022-04-04 00:44:52 -0700

    Tab sizes

--- dumps/calculate.sh
+++ dumps/calculate.sh
@@ -8,41 +8,41 @@ printf "Dump,Iter1,Iter2,Iter3,AverageTime\n" >> output.csv
 
 for dir in ethernet wifi mobile
 do
-    echo "$dir"
+  echo "$dir"
 
-    for subdir in "$dir"/*
+  for subdir in "$dir"/*
     do
-        echo -n "$subdir,"
+      echo -n "$subdir,"
 
-        times=()
+      times=()
 
-        for pcap in "$subdir"/*
-        do
-#             echo "$pcap"
+      for pcap in "$subdir"/*
+      do
+        # echo "$pcap"
 
-            # get start and end of network flow
-            timestamps=$(tshark -r "$pcap" -2 -R "ip.src == 10.0.0.1 || ip.src == 10.0.0.2" -T fields -e _ws.col.Time | sed -e 1b -e '$!d' | tr '\n' ' ')
+        # get start and end of network flow
+        timestamps=$(tshark -r "$pcap" -2 -R "ip.src == 10.0.0.1 || ip.src == 10.0.0.2" -T fields -e _ws.col.Time | sed -e 1b -e '$!d' | tr '\n' ' ')
 
-            # subtract the start and end to get total time of network flow
-            if [[ $timestamps =~ $regex ]]
-            then
-                ts1="${BASH_REMATCH[1]}"
-                ts2="${BASH_REMATCH[2]}"
-                time=`awk '{printf "%.9f\n", $1-$2}' <<< "$ts2 $ts1"`
-                times+=( $time )
-            else
-                echo "Error!"
-                exit 1
-            fi
-        done
+        # subtract the start and end to get total time of network flow
+        if [[ $timestamps =~ $regex ]]
+        then
+          ts1="${BASH_REMATCH[1]}"
+          ts2="${BASH_REMATCH[2]}"
+          time=`awk '{printf "%.9f\n", $1-$2}' <<< "$ts2 $ts1"`
+          times+=( $time )
+        else
+          echo "Error!"
+          exit 1
+        fi
+      done
 
-        # get average of captures
-        average=`awk '{printf "%.9f\n", ($1+$2+$3)/3}' <<< "${times[0]} ${times[1]} ${times[2]}"`
+    # get average of captures
+    average=`awk '{printf "%.9f\n", ($1+$2+$3)/3}' <<< "${times[0]} ${times[1]} ${times[2]}"`
 
-        printf '%s,%s,%s,%s\n' "${times[0]}" "${times[1]}" "${times[2]}" "$average"
-        printf '%s,%s,%s,%s,%s\n' "$subdir" "${times[0]}" "${times[1]}" "${times[2]}" "$average" >> output.csv
+    printf '%s,%s,%s,%s\n' "${times[0]}" "${times[1]}" "${times[2]}" "$average"
+    printf '%s,%s,%s,%s,%s\n' "$subdir" "${times[0]}" "${times[1]}" "${times[2]}" "$average" >> output.csv
 
-    done
+  done
 
-    echo
+  echo
 done
